57635cce0c63ea89f413734ffcb7559a7b666d63
vuongvgc <vuongvgc@gmail.com>
2020-11-04 18:18:11 +0700

    create  project social app

---
+++ src/app/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <section>
+        <h1 className="header__primary">Victor Social</h1>
+
+        <div className="navContent">
+          <div className="navLinks"></div>
+        </div>
+      </section>
+    </nav>
+  )
+}
